b1a2c1a3d6864c7a111c10d098d9d51bd2d5d31e
socproducts <46232536+socproducts@users.noreply.github.com>
2020-03-22 14:53:36 -0400

    housekeeping

--- models.py
+++ models.py
@@ -1,2 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
+from datetime import datetime
+
+
+class Log(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    stream = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
